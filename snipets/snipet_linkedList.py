24833bbd6acadb6e97d530d97b5b186e332d3413
0-tree <arthur.roullier@yahoo.com>
2018-12-13 10:04:16 +0100

    snip: update linkedlist

--- snipets/snipet_linkedList.py
+++ snipets/snipet_linkedList.py
@@ -3,6 +3,8 @@
 
 #%%
 
+#- first attempt
+
 class Node:
   
 	def __init__(self):
@@ -59,4 +61,97 @@ ll.print()
 print(ll.len)
 
 
-			
+#- second attempt, lighter
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList(object):
+    def __init__(self, value=None):
+        if value is not None:
+            value = Node(value)
+        self.head = value
+
+    def append(self, value):
+        newNode = Node(value)
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = newNode
+        else:
+            self.head = newNode
+
+    def get_position(self, position):
+        counter = 0
+        current = self.head
+        if position < 0:
+            return None
+        while current and counter <= position:
+            if counter == position:
+                return current.value
+            current = current.next
+            counter += 1
+        return None
+
+    def insert(self, value, position):
+        newNode = Node(value)
+        counter = 0
+        current = self.head
+        if position > 0:
+            while current and counter < position:
+                if counter == position - 1:
+                    newNode.next = current.next
+                    current.next = newNode
+                current = current.next
+                counter += 1
+        elif position == 0:
+            newNode.next = self.head
+            self.head = newNode
+
+    def delete(self, value):
+        current = self.head
+        previous = None
+        while current.value != value and current.next:
+            previous = current
+            current = current.next
+        if current.value == value:
+            if previous:
+                previous.next = current.next
+            else:
+                self.head = current.next
+
+# Test cases
+# Set up some Elements
+e1 = 0
+e2 = 'one'
+e3 = 2
+e4 = 'three'
+
+# Start setting up a LinkedList
+ll = LinkedList(e1)
+ll.append(e2)
+ll.append(e3)
+# ll.append(e4)
+
+# Test get_position
+# Should print 2
+print(ll.head.next.next.value)
+# Should also print 2
+print(ll.get_position(2))
+
+# Test insert
+ll.insert(e4,2)
+# Should print three now
+print(ll.get_position(2))
+
+# Test delete
+ll.delete('one')
+# Should print 0 now
+print(ll.get_position(0))
+# Should print three now
+print(ll.get_position(1))
+# Should print 2 now
+print(ll.get_position(2))
